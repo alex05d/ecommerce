65cb7f47bc32fbf87139a5408432dc3dc61f3a6b
Alexiss Diaz <alexiss.diaz@live.com>
2020-09-16 09:45:43 -0500

    added components and router path

--- react-store/src/App.js
+++ react-store/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">column number one</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home" />
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ react-store/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>hello from navbar</h3>
+            </div>
+        )
+    }
+}
