6d7ef6e938d261d12c9cfd0fe046c20324888261
Alexiss Diaz <alexiss.diaz@live.com>
2020-09-18 14:57:37 -0500

    added navbar styling and button conponent styling

--- react-store/src/components/ProductList.js
+++ react-store/src/components/ProductList.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title'
 
 export default class ProductList extends Component {
+    state = {
+        products: []
+    }
+
     render() {
         return (
-            <div>
-                <Product></Product>
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products" />
+                        <div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+
+            // <Product></Product>
+
         )
     }
 }
