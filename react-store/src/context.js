7e1a70f33c08dc32734ca659c69d1aecc96d6dc3
Alexiss Diaz <alexiss.diaz@live.com>
2020-12-15 17:11:11 -0600

    added place holder for new methods in contex.js

--- react-store/src/context.js
+++ react-store/src/context.js
@@ -12,6 +12,9 @@ class ProductProvider extends Component {
         cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0,
 
     };
     // adding this will insure i get the correct info from the data not just a refrence 
@@ -85,6 +88,21 @@ class ProductProvider extends Component {
     //         console.log('data products :', storeProducts[0].inCart);
     //     })
     // }
+    increment = (id) => {
+        console.log('this is increment method');
+    };
+    decrement = (id) => {
+        console.log('this is decrement method');
+    };
+
+    removeItem = (id) => {
+        console.log('item removed')
+    };
+
+    clearCart = () => {
+        console.log('cart cleared')
+    }
+
     render() {
         return (
             <ProductContext.Provider value={{
@@ -92,7 +110,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {this.props.children}
 
